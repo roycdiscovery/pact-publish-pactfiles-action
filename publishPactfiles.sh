8f504821567fd9e3d1ddf9b24352dfc3bbe98966
Roy Collings <roy_collings@discovery.com>
2022-04-22 16:45:46 +0200

    Added unit tests

--- publishPactfiles.sh
+++ publishPactfiles.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+MISSING=()
+[ ! "$pact_broker" ] && MISSING+=( "pact_broker" )
+[ ! "$pact_broker_token" ] && MISSING+=( "pact_broker_token" )
+[ ! "$application_name" ] && MISSING+=( "application_name" )
+[ ! "$version" ] && MISSING+=( "version" )
+[ ! "$pactfiles" ] && MISSING+=( "pactfiles" )
+
+if [ ${#MISSING[@]} -gt 0 ]
+then
+  echo "ERROR: The following environment variables are not set:"
+  printf '\t%s\n' "${MISSING[@]}"
+  exit 1
+fi
+
 echo """
 pact_broker: $pact_broker
 pact_broker_token: $pact_broker_token
